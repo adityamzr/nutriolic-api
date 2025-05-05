f3f8b5db2eae49033958e5c39607c839328dc248
Aditya Muhammad Zaini <adityamzainir@gmail.com>
2025-05-05 10:21:34 +0700

    feat(migration): new migration to add p9 field

---
+++ prisma/migrations/20250505032055_add_p9_field/migration.sql
@@ -0,0 +1,11 @@
+-- AlterTable
+ALTER TABLE "GiziResponse" ADD COLUMN     "p9" JSONB;
+
+-- AlterTable
+ALTER TABLE "KaloriResponse" ADD COLUMN     "p9" JSONB;
+
+-- AlterTable
+ALTER TABLE "NutrisiResponse" ADD COLUMN     "p9" JSONB;
+
+-- AlterTable
+ALTER TABLE "PiringkuResponse" ADD COLUMN     "p9" JSONB;
